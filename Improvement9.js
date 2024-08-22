5e6401203ab76063f24476bb5e21a78e07f7d37f
Kamikazi-Deborah <deborahkamikazi9@gmail.com>
2024-08-22 10:05:18 +0200

    Improvement9

--- Improvement9.js
+++ Improvement9.js
@@ -1,11 +1,8 @@
 function calculateMatrixSum(matrix) {
-  const finalmatrix = [];
-  matrix.map((el) => {
-    el = el.reduce((a, b) => a + b, 0);
-    finalmatrix.push(el);
-  });
-  return ms.reduce((a, b) => a + b);
+ return matrix.map((el) => {
+     return el.reduce((a, b) => a + b, 0);
+    }).reduce((a, b) => a + b);
 }
 console.log(
-  calculateMatrixSum([S[(1, 2, 3)], [4, 5, 6], [7, 8, 9], [12, 3, 4, 5]])
+  calculateMatrixSum([[1, 2, 3], [4, 5, 6], [7, 8, 9], [12, 3, 4, 5]])
 );
